0f3d84c8f3b22426598b83a11d6f1c9988509853
VeryStone69 <68245371+VeryStone69@users.noreply.github.com>
2022-07-16 15:57:20 +0200

    add constructing strings with variables

--- Basic JavaScript/concatenating-strings-with-plus-operator.js
+++
@@ -1,5 +0,0 @@
-const myStr = "This is the start. " + "This is the end."; 
-
-//Concatenating Strings with the Plus Equals Operator
-let myStr = "This is the first sentence. ";
-myStr += "This is the second sentence."
---
+++ Basic JavaScript/strings.js
@@ -0,0 +1,9 @@
+const myStr1 = "This is the start. " + "This is the end."; 
+
+//Concatenating Strings with the Plus Equals Operator
+let myStr2 = "This is the first sentence. ";
+myStr2 += "This is the second sentence."
+
+//Constructing Strings with Variables
+const myName = "Aliaksandr";
+const myStr3 = "My name is " + myName + "and I am well!";
