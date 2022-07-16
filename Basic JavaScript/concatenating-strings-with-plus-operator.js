685ade952607fe2c28020949aaab025721a41477
VeryStone69 <68245371+VeryStone69@users.noreply.github.com>
2022-07-16 15:51:51 +0200

    add plus equals operator

--- Basic JavaScript/concatenating-strings-with-plus-operator.js
+++ Basic JavaScript/concatenating-strings-with-plus-operator.js
@@ -1,1 +1,5 @@
-const myStr = "This is the start. " + "This is the end."; // Change this line
+const myStr = "This is the start. " + "This is the end."; 
+
+//Concatenating Strings with the Plus Equals Operator
+let myStr = "This is the first sentence. ";
+myStr += "This is the second sentence."
