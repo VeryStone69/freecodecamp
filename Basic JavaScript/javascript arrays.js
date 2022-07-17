19a0fecdc8fda804026aef4d6e8721e0373f771b
VeryStone69 <68245371+VeryStone69@users.noreply.github.com>
2022-07-17 17:45:41 +0200

    Modify Array Data With Indexes

--- Basic JavaScript/javascript arrays.js
+++ Basic JavaScript/javascript arrays.js
@@ -6,4 +6,8 @@ const myArray2 = [['hello'],[505]];
 
 //Access Array Data with Indexes
 const myArray3 = [50, 60, 70];
-let myData = myArray[0];
+let myData = myArray3[0];
+
+//Modify Array Data With Indexes
+const myArray4 = [18, 64, 99];
+myArray4[0] = 45;
