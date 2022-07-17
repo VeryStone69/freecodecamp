f23248c484322708b399b978660ce4acbe35e690
VeryStone69 <68245371+VeryStone69@users.noreply.github.com>
2022-07-17 16:46:09 +0200

    find last character
    Use Bracket Notation to Find the Nth-to-Last Character in a String

--- Basic JavaScript/use-bracket-notation-to-find-the-last-character-in-a-string.js
+++ Basic JavaScript/use-bracket-notation-to-find-the-last-character-in-a-string.js
@@ -1,5 +1,7 @@
-// Setup
+// Use Bracket Notation to Find the Nth Character in a String
 const lastName = "Lovelace";
-
-// Only change code below this line
 const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
+
+//Use Bracket Notation to Find the Nth-to-Last Character in a String
+const lastName1 = "Lovelace";
+const secondToLastLetterOfLastName = lastName[lastName.length - 2];
