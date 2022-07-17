9189089207ccc05d253e816c1dafb000952b0758
VeryStone69 <68245371+VeryStone69@users.noreply.github.com>
2022-07-17 21:44:22 +0200

    Passing Values to Functions with Arguments

--- Basic JavaScript/Reusable JavaScript with Functions.js
+++ Basic JavaScript/Reusable JavaScript with Functions.js
@@ -1,5 +1,11 @@
 //Write Reusable JavaScript with Functions
 function reusableFunction() {
-  console.log('Hi World')
-}
+    console.log('Hi World')
+  }
 reusableFunction(); // in console: Hi World
+
+//Passing Values to Functions with Arguments
+function functionWithArgs(a,b) {
+  console.log(a+b);
+  }
+functionWithArgs(5,5);
