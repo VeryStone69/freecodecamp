a89a3e4a9336cd333dc8e49aa5cebdcb8282a50e
VeryStone69 <68245371+VeryStone69@users.noreply.github.com>
2022-07-17 21:48:44 +0200

    update: Return a Value from a Function with Return

--- Basic JavaScript/Reusable JavaScript with Functions.js
+++ Basic JavaScript/Reusable JavaScript with Functions.js
@@ -6,6 +6,12 @@ reusableFunction(); // in console: Hi World
 
 //Passing Values to Functions with Arguments
 function functionWithArgs(a,b) {
-  console.log(a+b);
+    console.log(a+b);
   }
 functionWithArgs(5,5);
+
+//Return a Value from a Function with Return
+function timesFive(num){
+    return num * 5;
+  }
+const answer = timesFive(5);
