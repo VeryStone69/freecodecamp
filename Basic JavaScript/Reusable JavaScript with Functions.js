13516c53732e790d4228078404a75058dbe4d885
VeryStone69 <68245371+VeryStone69@users.noreply.github.com>
2022-07-17 21:54:10 +0200

    update: Global Scope and Functions

--- Basic JavaScript/Reusable JavaScript with Functions.js
+++ Basic JavaScript/Reusable JavaScript with Functions.js
@@ -15,3 +15,21 @@ function timesFive(num){
     return num * 5;
   }
 const answer = timesFive(5);
+
+//Global Scope and Functions
+let myGlobal = 10;
+
+function fun1() {
+    oopsGlobal = 5;
+ }
+
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
