759562ceb4ad519ddd9018201313a0e0f50c462f
VeryStone69 <68245371+VeryStone69@users.noreply.github.com>
2022-07-17 17:50:41 +0200

    Access Multi-Dimensional Arrays With Indexes

--- Basic JavaScript/javascript arrays.js
+++ Basic JavaScript/javascript arrays.js
@@ -6,8 +6,18 @@ const myArray2 = [['hello'],[505]];
 
 //Access Array Data with Indexes
 const myArray3 = [50, 60, 70];
-let myData = myArray3[0];
+let myData3 = myArray3[0];
 
 //Modify Array Data With Indexes
 const myArray4 = [18, 64, 99];
 myArray4[0] = 45;
+
+//Access Multi-Dimensional Arrays With Indexes
+const myArray5 = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [[10, 11, 12], 13, 14],
+];
+
+const myData5 = myArray5[2][1];
