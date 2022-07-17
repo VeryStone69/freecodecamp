58c571790f755b046fa293f4d7398b832354b2dd
VeryStone69 <68245371+VeryStone69@users.noreply.github.com>
2022-07-17 17:53:49 +0200

    add comment for solution tasks

--- Basic JavaScript/javascript arrays.js
+++ Basic JavaScript/javascript arrays.js
@@ -6,11 +6,11 @@ const myArray2 = [['hello'],[505]];
 
 //Access Array Data with Indexes
 const myArray3 = [50, 60, 70];
-let myData3 = myArray3[0];
+let myData3 = myArray3[0]; // myData3 = 50
 
 //Modify Array Data With Indexes
 const myArray4 = [18, 64, 99];
-myArray4[0] = 45;
+myArray4[0] = 45; //const myArray4 = [45, 64, 99];
 
 //Access Multi-Dimensional Arrays With Indexes
 const myArray5 = [
@@ -20,4 +20,4 @@ const myArray5 = [
   [[10, 11, 12], 13, 14],
 ];
 
-const myData5 = myArray5[2][1];
+const myData5 = myArray5[2][1]; //myData5 =8;
