061cae26f22c267330779b6bca09b50bd1ec8edf
Aliaksandr <68245371+VeryStone69@users.noreply.github.com>
2022-08-14 20:05:57 +0200

    update Complete a Promise with resolve and reject
    Add the catch method to your promise. Use error as the parameter of its callback function and log error to the console.

--- ES6/complete-a-promise-with-resolve-and-reject.js
+++ ES6/complete-a-promise-with-resolve-and-reject.js
@@ -29,3 +29,7 @@ const makeServerRequest = new Promise((resolve, reject) => {
 makeServerRequest.then(result => {
   console.log(result)
 })
+
+makeServerRequest.catch(error => {
+  console.log(error);
+})
