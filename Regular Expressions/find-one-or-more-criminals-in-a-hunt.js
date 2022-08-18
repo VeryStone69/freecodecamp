08253f8335b810ff6a9fda2d2daf8899614ce52d
Aliaksandr <68245371+VeryStone69@users.noreply.github.com>
2022-08-18 07:49:07 +0200

    add find one or more criminals in a hunt

---
+++ Regular Expressions/find-one-or-more-criminals-in-a-hunt.js
@@ -0,0 +1,8 @@
+//===== Find One or More Criminals in a Hunt
+// Time to pause and test your new regex writing skills. A group of criminals escaped from jail and ran away, but you don't know how many. However, you do know that they stay close together when they are around other people. You are responsible for finding all of the criminals at once.
+
+//===== Exercise
+// Write a greedy regex that finds one or more criminals within a group of other people. A criminal is represented by the capital letter C.
+
+//===== My solution
+let reCriminals = /C+/;
